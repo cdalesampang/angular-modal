7eb491fb004a3b4f4c897966f5c0b35c35f53ee5
cdalesampang <cdalesampang@outlook.com>
2017-06-28 16:34:03 +0800

    Add files via upload
    Added JS Files

---
+++ modal-template.js
@@ -0,0 +1,24 @@
+﻿(function () {
+
+    'use strict';
+
+    angular.module('Asclepius')
+        .controller("RegistrationModalController", RegistrationModalController);
+
+    RegistrationModalController.$inject = ["$scope", "$uibModalInstance", "RegistrationService", "data"];
+    function RegistrationModalController($scope, $uibModalInstance, RegistrationService, data) {
+
+        $scope.data = angular.copy(data);
+
+        $scope.save = function () {
+            //TODO 
+            //Call to Service to update
+            $uibModalInstance.close($scope.data);
+        };
+
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+
+    }
+})();
---
+++ modal.service.js
@@ -0,0 +1,41 @@
+﻿(function () {
+
+    'use strict';
+
+    angular.module("Asclepius")
+        .service("ModalService",ModalService);
+
+    ModalService.$inject = ["$uibModal", "$log"];
+
+    function ModalService($uibModal, $log) {
+        var modal = {};
+        
+        modal.open = function (size,data,template,callback) {
+            var modalInstance = $uibModal.open({
+                size: size,
+                animation: true,
+                backdrop: true,
+                templateUrl: template,
+                controller: 'RegistrationModalController',
+                resolve: {
+                    data: function () {
+                        return data ;
+                    }
+                }
+            });
+            modalInstance.result.then(function (response) {
+                debugger;
+                //TODO 
+                //Refresh the table
+                callback(response);
+                $log.info(response);
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+        return modal;
+    }
+
+})();
+
